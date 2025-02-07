af85ae7f39aa63327086c68b9028c908dab288a1
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-02-07 23:26:21 +0300

    Using async functions to read files

---
+++ callback.js
@@ -0,0 +1,8 @@
+import fs from "fs";
+import path from "path";
+
+const callbackFetch = (callback) => {
+    const FilePath = path.join(__dirname, "sample1.json"); //The FilePath variable stores the directory to the json file
+
+    
+}
