904ff419047dd5311a3b2f594e87484bcd986f19
Y.A.S.1 <133089151+YAS-1@users.noreply.github.com>
2025-02-08 00:01:23 +0300

    callbackMethod to fetch data

--- callback.js
+++ callback.js
@@ -1,8 +1,23 @@
-import fs from "fs";
-import path from "path";
+
+const fs = require("fs");
+const path = require("path")
+
 
 const callbackFetch = (callback) => {
     const FilePath = path.join(__dirname, "sample1.json"); //The FilePath variable stores the directory to the json file
 
-    
+    fs.readFile(FilePath, 'utf-8', (error, data) => { // Using the fs.readFile to read the file in the FilePath directory
+        if (error){
+            return callback(error, null); // if there is an error reading the file it is passed to the callback and exited
+        }
+
+        try {
+            const ParsedData = JSON.parse(data);
+            return callback(null, ParsedData); // if the data is parsed successfully it is passed to the callback
+        } catch (ParsedError) {
+            callback(ParsedError, null); // if the parsing is unsuccessful the error is caught and passed to the callback
+        }
+    });
 }
+
+module.exports = callbackFetch;
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-import express from "express";
+const express = require("express");
+
+const callbackFetch = require("./callback");
+
 
 const app = express();
 
 const port = 3000;
 
 app.listen(port, (req, res)=>{
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}\n`);
+
+    // The callback method to fetch data
+    callbackFetch((error, data) =>{
+        if(error){
+            console.log(error);
+        }
+        else{
+            console.log('Using the callback method data in Sample1.json:',data);
+        }
+    })
 });
